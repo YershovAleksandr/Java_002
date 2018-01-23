56f538d26f6c1ca3428e9b0b565541f87edf2a08
hp <namstudionsk@gmail.com>
2018-01-24 02:51:12 +0700

    Add all files

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -22,40 +22,31 @@ public class Main {
         long seconds;
         long distance;
 
-        lightspeed = 186000;    //light speed miles in seconds
+        boolean b;
 
-        days = 1000;
+        b = false;
+        System.out.println("b = " + b);
 
-        seconds = days * 24 * 60 * 60;
+        b = true;
+        System.out.println("b = " + b);
 
-        distance = lightspeed * seconds;
+        if (b)
+        {
+            System.out.println("true");
+        }
 
-        System.out.print("За " + days);
-        System.out.print(" днец свет пройдёт около ");
-        System.out.println(distance + " миль.");
+        b = !b;
 
+        if (!b)
+        {
+            System.out.println("false");
+        }
 
-        double pi, r , a;
+        System.out.println("Wtf " + (5 > 1));
 
-        r = 10.8;
-        pi = 3.1416;
-        a = pi * r * r;
+        int x = 123_456_789_0;
 
-        System.out.println("Площадь круга равна " + a);
-
-        char ch1;
-        char ch2;
-
-        ch1 = 88;   //code of 'X'
-        ch2 = 'Y';
-
-        System.out.print("ch1 and ch2: ");
-        System.out.println(ch1 + " " + ch2);
-
-        ch2++;
-        System.out.println("ch2 =  " + ch2);
-
-
+        System.out.println("X = " + x);
 
     }
 }
